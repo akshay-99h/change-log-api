ab9f6adeaf09f609b46552da3e70ea8566ccb2fb
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2023-04-15 19:17:20 +0530

    add a few new routers delete old unessential handlers

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import { body, validationResult } from "express-validator" 
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -11,33 +12,43 @@ router.get('/product', (req, res) => {
     res.json({message: "hello"})
 })
 router.get('/product/:id', () => { })
-router.put('/product/:id', body('name').isString(), (req, res) => {
-
-    const errors = validationResult(req);
-    console.log(errors);
-
-    if (!errors.isEmpty()) {
-        res.status(400);
-        res.json({ errors: errors.array() });
-    }
-});
-router.post('/product', () => { })
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => { });
+router.post('/product', body('name').isString(), handleInputErrors, (req, res) => { })
 router.delete('/product/:id', () => { })
 
 // Update
 
 router.get('/update', () => { })
 router.get('/update/:id', () => { })
-router.put('/update/:id', () => { })
-router.post('/update', () => { })
+router.put('/update/:id',
+    body("title").optional(),
+    body("body").optional(),
+    body('status').isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+    body("version").optional(),
+    () => { }
+)
+router.post('/update',
+    body("title").exists().isString(),
+    body("body").exists().isString(),
+    () => { }
+)
 router.delete('/update/:id', () => { })
 
 // Update Point
 
 router.get('/updatepoint', () => { })
 router.get('/updatepoint/:id', () => { })
-router.put('/updatepoint/:id', () => { })
-router.post('/updatepoint', () => { })
+router.put('/updatepoint/:id',
+    body("name").optional().isString(),
+    body("description").optional().isString(),
+    () => { }
+)
+router.post('/updatepoint',
+    body("name").isString(),
+    body("description").isString(),
+    body("updateId").exists().isString(),
+    () => { }
+)
 router.delete('/updatepoint/:id', () => { })
 
 export default router;
